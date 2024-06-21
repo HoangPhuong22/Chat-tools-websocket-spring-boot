d030efcdead3c658323efb487dec7adb60560837
Hoàng Phương <101681993+HoangPhuong22@users.noreply.github.com>
2024-06-21 17:47:59 +0700

    create project

---
+++ src/main/java/vn/study/ChatTools/ChatToolsApplication.java
@@ -0,0 +1,13 @@
+package vn.study.ChatTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatToolsApplication.class, args);
+	}
+
+}
